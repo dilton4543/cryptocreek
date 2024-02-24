52ad0b3956ee64ccb0cce3370388024edf96ca4c
dilton4543 <loteesenwah.17@gmail.com>
2024-02-24 10:18:34 +0100

    added a search functionality for the news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,22 +1,41 @@
-
-import React from 'react';
-import { Typography, Row, Col, Card } from 'antd';
+import React, { useState } from 'react';
+import { Typography, Row, Col, Card, Avatar, Select } from 'antd';
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
-const { Title } = Typography;
+const { Title,Text } = Typography;
+const {Option} = Select;
 
 const News = ({ simplified }) => {
-  const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({ newsCategory: 'coindesk', count: simplified ? 10 : 100 }); //newsCategory can be changed to theguardian,coindesk, bsc
+
+  const [newsCategory, setNewsCategory]= useState('coindesk'); //since the initail state is coindesk here, we dont have to hardcode it below again.(newsCategory:'coindesk')
+  const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 10 : 100 }); //newsCategory can be changed to theguardian,coindesk, bsc
+  const {data} = useGetCryptosQuery(100);
 
   if (isFetching) return <div>Loading...</div>;
 
   // If simplified, slice the array to get only the first 10 items
-  const newsToRender = simplified ? cryptoNews?.data?.slice(0, 10) : cryptoNews?.data;
+  const newsToRender = simplified ? cryptoNews?.data?.slice(0, 9) : cryptoNews?.data;
 
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select 
+            showSearch 
+            className='select-news'
+            placeholder="select a crypto"
+            optionFilterProp='children'
+            onChange={(value)=> setNewsCategory(value)}
+            filterOption={(input, option)=>option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+          >
+              <Option value="coindesk"> Cryptocurrency </Option>
+              {data?.data?.coins.map((coin)=> <Option value={coin.name}>{coin.name}</Option>)}
+          </Select>
+        </Col>
+      )}
       {newsToRender?.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={news.url}>
           <Card hoverable>
