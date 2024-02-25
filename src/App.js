990446ab2401f8f4862396ad84239ef0372c9691
dilton4543 <loteesenwah.17@gmail.com>
2024-02-25 19:39:38 +0100

    created menuoutlined for mobile view

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { Routes, Route, Link} from 'react-router-dom'; //versions 5 below use Switch not Routes
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components'; // importing our Navbar component from the components folder because we have already exported it in our (components/index.js) folder.
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails, Profile } from './components'; // importing our Navbar component from the components folder because we have already exported it in our (components/index.js) folder.
 
 import './App.css';
 // import 'antd/dist/antd.css';
@@ -40,6 +40,10 @@ const App = () => { //Routes
               <Route  
               exact path="/news"
               element={<News />}
+              />
+              <Route  
+              exact path="/profile"
+              element={<Profile />}
               />
               </Routes>
           </div>
--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const Exchanges = () => {
   return (
     <div>
       <h1>Welcome to Exchanges page</h1>
+      <div>
+        <Link to="/">click me</Link>
+      </div>
     </div>
   )
 }
