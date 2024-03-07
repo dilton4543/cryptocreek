6ace769a5670e7437282b75b3460f0d50b413972
dilton4543 <loteesenwah.17@gmail.com>
2024-03-07 23:47:59 +0100

    added popup notification for unauthenticated users

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,22 @@
-// PrivateRoute.jsx
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { notification } from 'antd'; // Import notification
 
 const PrivateRoute = ({ children, isLoggedIn }) => {
-  return isLoggedIn ? children : <Navigate to="/" replace />;
+  if (!isLoggedIn) {
+    // Trigger a notification before redirecting
+    notification.open({
+      message: 'Authentication Required',
+      description: 'Please login to your account to access this page.',
+      onClick: () => {
+        console.log('Notification Clicked!');
+      },
+    });
+
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
 };
 
 export default PrivateRoute;
